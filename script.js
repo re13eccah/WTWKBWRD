aecffc4cd8a143851bcbb8cad01f9c675f5e9eff
re13eccah <20230722@mywhitecliffe.com>
2024-06-30 21:52:45 +1200

    g

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ function changeImage() {
     carouselImage.src = images[currentIndex];
 }
 
-setInterval(changeImage, 500); // Change image every 3 seconds
-
 // Initial setup to show the first image
 carouselImage.src = images[0];
-carouselImage.classList.add('active');
+
+setInterval(changeImage, 500); // Change image every 3 seconds
